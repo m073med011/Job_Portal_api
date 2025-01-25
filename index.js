de809914e27bb925b7436f75e22dfb6fa2c437e1
m073med011 <166988534+m073med011@users.noreply.github.com>
2025-01-25 17:34:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,23 +8,28 @@ const app = express();
 dotenv.config();
 const port = process.env.PORT || 8080;
 
-
-
 // Database connection
 connectDB();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
-app.use(cookieParser())
+
+// Configure CORS to allow access from all origins
+app.use(cors({
+  origin: "*", // Allow all origins
+  methods: "GET,POST,PUT,DELETE,PATCH,OPTIONS",
+  allowedHeaders: "Content-Type,Authorization",
+}));
+
+app.use(cookieParser());
 
 // Import routes
 import jobRoutes from "./routes/jobRoutes.js";
-import userRoutes from "./routes/userRoutes.js"
-import applicationRoutes from "./routes/applicationRoutes.js"
-import recruiterRoutes from "./routes/recruiterRoutes.js"
-import fileUploadRoute from './routes/fileUploadRoute.js'
-import Auth from './routes/Auth.js'
+import userRoutes from "./routes/userRoutes.js";
+import applicationRoutes from "./routes/applicationRoutes.js";
+import recruiterRoutes from "./routes/recruiterRoutes.js";
+import fileUploadRoute from './routes/fileUploadRoute.js';
+import Auth from './routes/Auth.js';
 
 // Use routes
 app.use("/jobs", jobRoutes);
@@ -32,19 +37,19 @@ app.use("/users", userRoutes);
 app.use("/application", applicationRoutes);
 app.use("/recruiter", recruiterRoutes);
 app.use("/", fileUploadRoute);
-app.use("/auth", Auth)
+app.use("/auth", Auth);
 
 // Routes
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
 
+// Handle undefined routes
 app.get("*", (req, res) => {
   res.redirect("/");
-}
-);
+});
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
